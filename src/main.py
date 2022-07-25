109eda8d52af1c990beb4178289bafd19fb45565
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-07-25 11:47:26 +0200

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,67 @@
+import random
+import string
+from tkinter import *
+import tkinter as tk
+
+fenetre = tk.Tk()
+fenetre.geometry("400x250")
+
+
+def test(l):
+    l = int(l.get())
+    char = str()
+    if var_low.get() == 1:
+        char = char + string.ascii_lowercase
+    if var_up.get() ==1:
+        char = char + string.ascii_uppercase
+    if var_num.get() ==1:
+        char = char + string.digits
+    if var_pun.get() ==1:
+        char = char + string.punctuation
+
+    mdp = ''.join(random.choice(char) for i in range(l))
+    return mdp
+
+
+def setTextInput(text):
+    textEntry.set(text)
+
+textEntry = tk.StringVar()
+
+
+champ_label = Label(fenetre, text="Générateur de mots de passes")
+
+champ_label.pack()
+
+mdp = tk.Entry(fenetre, textvariable= textEntry)
+mdp.configure(state="readonly")
+mdp.pack()
+
+
+taille = Label(fenetre, text="Entrez la taille du mot de passe désiré")
+taille.pack(pady=5)
+l =Entry(fenetre)
+l.insert(0, "8")
+l.pack()
+
+var_low = IntVar()
+case_low = Checkbutton(fenetre, text="Caractères minuscules", variable=var_low)
+case_low.pack()
+
+var_up = IntVar()
+case_up = Checkbutton(fenetre, text="Caractères majuscules", variable=var_up)
+case_up.pack()
+
+var_num = IntVar()
+case_num = Checkbutton(fenetre, text="Chiffres (0-9)", variable=var_num)
+case_num.pack()
+
+var_pun = IntVar()
+case_pun = Checkbutton(fenetre, text="Caractères spéciaux", variable=var_pun)
+case_pun.pack()
+
+bouton_générer = Button(fenetre, text="Générer", command = lambda:setTextInput(test(l)))
+bouton_générer.pack(pady=10)
+
+fenetre.mainloop()
+    
